e936e55dc38aab1c3dd7f16cd08daa38243cb081
Daniel Tschinder <daniel.tschinder@researchgate.net>
2019-11-29 15:18:40 +0100

    feat(eslint): Automatically create config for editor support
    This creates the file `.eslintrc.js` if no eslint config is available to add support
    for editors.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,2 +1,2 @@
-// Keep this file for editor support
+// This file was created by spire-plugin-eslint for editor support
 module.exports = require('spire-plugin-eslint/config');
--- packages/spire-plugin-eslint/index.js
+++ packages/spire-plugin-eslint/index.js
@@ -1,33 +1,66 @@
 const execa = require('execa');
 const SpireError = require('spire/error');
+const { writeFile, readFile } = require('fs-extra');
+const { join } = require('path');
 
 function eslint(
   { setState, getState, hasFile, hasPackageProp },
   {
     command = 'lint',
-    config: defaultEslintConfig = require.resolve('spire-plugin-eslint/config'),
+    config: defaultEslintConfig = 'spire-plugin-eslint/config',
+    autosetEslintConfig = true,
     allowCustomConfig = true,
     eslintIgnore: defaultEslintIgnore = '.gitignore',
     allowCustomIgnore = true,
     glob = '*.js',
   }
 ) {
+  async function hasCustomEslintConfig() {
+    return (
+      (await hasFile('.eslintrc')) ||
+      (await hasFile('.eslintrc.js')) ||
+      (await hasFile('.eslintrc.json')) ||
+      (await hasFile('.eslintrc.yaml')) ||
+      (await hasFile('.eslintrc.yml')) ||
+      (await hasPackageProp('eslintConfig'))
+    );
+  }
   return {
     name: 'spire-plugin-eslint',
     command,
     description: 'lint files with ESLint',
-    async setup({ cli, argv }) {
+    async postinstall({ cwd, logger }) {
+      if (autosetEslintConfig) {
+        const hasCustomConfig = await hasCustomEslintConfig();
+        const configFile = join(cwd, '.eslintrc.js');
+        if (hasCustomConfig) {
+          if (await hasFile('.eslintrc.js')) {
+            const currentContent = await readFile(configFile, 'UTF-8');
+            if (!currentContent.includes(defaultEslintConfig)) {
+              return logger.warn(
+                'Attempted to set ESLint config but it already exists. Please ensure existing config re-exports `%s`.',
+                defaultEslintConfig
+              );
+            }
+          }
+        }
+        await writeFile(
+          configFile,
+          `// This file was created by spire-plugin-eslint for editor support
+module.exports = require('${defaultEslintConfig}');`
+        );
+      }
+    },
+    async setup({ cli, argv, cwd }) {
       const hasCustomConfig =
-        argv.includes('--config') ||
-        (await hasFile('.eslintrc')) ||
-        (await hasFile('.eslintrc.js')) ||
-        (await hasFile('.eslintrc.json')) ||
-        (await hasFile('.eslintrc.yaml')) ||
-        (await hasPackageProp('eslintConfig'));
+        argv.includes('--config') || (await hasCustomEslintConfig());
       const eslintConfig =
         allowCustomConfig && hasCustomConfig
           ? []
-          : ['--config', defaultEslintConfig];
+          : [
+              '--config',
+              require.resolve(defaultEslintConfig, { paths: [__dirname, cwd] }),
+            ];
       const hasCustomIgnore =
         argv.includes('--ignore-path') ||
         (await hasFile('.eslintignore')) ||
