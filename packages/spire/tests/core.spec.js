e3f7876ad09d81543007514155ad6ef7c9a68b7c
Daniel Tschinder <daniel.tschinder@researchgate.net>
2019-11-29 14:28:10 +0100

    fix(spire): detect indentation when writing package.json file

---
+++ packages/spire/tests/core.spec.js
@@ -0,0 +1,27 @@
+const { createFixture } = require('spire-test-utils');
+const { readFile } = require('fs-extra');
+const { join } = require('path');
+const createCore = require('../lib/create-core');
+
+const getCore = cwd => {
+  return createCore({ cwd: cwd }, { setState: () => {}, getState: () => {} });
+};
+
+describe('spire', () => {
+  test.each(['  ', '    ', '   ', '\t'])(
+    'detects correct indent for "%s"',
+    async indent => {
+      const fixture = await createFixture({
+        'package.json': `{\n${indent}"name": "spire-test-hooks"\n}`,
+      });
+      const { setPackageProp } = getCore(fixture.cwd);
+      await setPackageProp('test', '123');
+      expect(
+        readFile(join(fixture.cwd, 'package.json'), 'UTF-8')
+      ).resolves.toBe(
+        `{\n${indent}"name": "spire-test-hooks",\n${indent}"test": "123"\n}\n`
+      );
+      await fixture.clean();
+    }
+  );
+});
